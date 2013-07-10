5c0d304b52a8381428915fbf74cdf8519ca040ed
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-07-10 00:23:36 -0400

    Initial commit and reloader in lib directory

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.0'
+
+gem 'sqlite3'
+
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+
+gem 'jquery-rails'
+
+#Added
+# gem 'journey'
+# gem 'puma'
+# gem 'rb-fsevent'
+
+# Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
+gem 'turbolinks'
+
+# Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
+gem 'jbuilder', '~> 1.2'
+
+group :doc do
+  # bundle exec rake doc:rails generates the API under doc/api.
+  gem 'sdoc', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LiveChattr::Application.config.secret_key_base = '9efe137066cb97a8ab5d49633d44df6318167d862cdf3b8ef66d01c2a9b3ff14b14f25edbd796ed3b10e96b3bad8a898e0d7f38b6cdf9b1157219eaa3aa849f4'
